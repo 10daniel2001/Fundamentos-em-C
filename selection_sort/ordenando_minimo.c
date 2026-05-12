c3201ff71cc19083b6440f46897cb9de540b4cba
10daniel2001 <cd38668cd@gmail.com>
2026-05-11 21:02:58 -0300

    Iniciando o metodo de ordenaçao

---
+++ selection_sort/ordenando_minimo.c
@@ -0,0 +1,7 @@
+/*Este codigo educacional e para o metodo de ordenaçao selection sort,
+  onde busca o menor elemento no array e coloca no lugar exato nada muito complexo
+  
+  iniciar com uma funçao onde preciso passar valores por refrencia essa funçao vai se chamar troca
+  ela vai fazer a troca exata no array quando necessario*/
+
+#include <stdio.h>
